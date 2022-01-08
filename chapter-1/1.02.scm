e880233475ee0270f882f79763dd5366ef56a8bd
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2022-01-08 13:45:38 -0600

    Add 1.02

---
+++ chapter-1/1.02.scm
@@ -0,0 +1,11 @@
+; Translate the following expression into prefix form:
+; (5 + 4 + (2 - (3 - (6 + 4/5)))) / (3(6 - 2)(2 - 7))
+;
+; -37/150 ~= -0.2466667
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+
+; 1 ]=> (load "chapter-1/1.02.scm")
+
+;Loading "chapter-1/1.02.scm"... done
+;Value: -37/150
